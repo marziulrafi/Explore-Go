b30283259842144370f1e5c0a91df5011c785d1f
K M Marziul Karim <marziulrafi@gmail.com>
2026-05-10 23:56:33 +0600

    function with return

--- Function/main.go
+++ Function/main.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
-func makeCoffee(kind string, isSugar bool) {
-	fmt.Printf("Making %s Coffee .....\n", kind)
-	fmt.Println("Sugar added : ", isSugar)
+// func makeCoffee(kind string, isSugar bool) {
+// 	fmt.Printf("Making %s Coffee .....\n", kind)
+// 	fmt.Println("Sugar added : ", isSugar)
+// }
+
+func makeCoffee(kind string) string {
+	coffee := fmt.Sprintf("%s Coffee!", kind)
+	return coffee
 }
 
 func main() {
-	makeCoffee("Espresso", false)
-	makeCoffee("Latte", true)
+	// makeCoffee("Espresso", false)
+	// makeCoffee("Latte", true)
+
+	myCoffee := makeCoffee("Cappuccino")
+	fmt.Println("I am having", myCoffee)
 }
